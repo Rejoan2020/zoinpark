1ec410566219f5332065fba90c8e89146fec1c6a
Rejoan2020 <rej.ash11@gmail.com>
2026-06-30 22:17:26 +0600

    feature/staking page is done

--- app/(menu)/staking/components/StakingNavbar.jsx
+++ app/(menu)/staking/components/StakingNavbar.jsx
@@ -1,15 +1,22 @@
+'use client'
 import React from 'react'
-import Image from 'next/image'
 
-export default function StakingNavbar() {
+
+export default function StakingNavbar({ handleClick, staking }) {
   return (
     <div className='text-secondaryText'>
       <div className='text-primaryText bg-[#032C2D] p-8 text-[12px] md:text-[16px] lg:text-[20px] xl:text-[24px]'>
         1 ZOIN = 0.010355 USDT
       </div>
       <div className='flex pt-8'>
-        <div className='border-b border-zinc-800 w-full flex justify-center pb-2'>Staking Packages</div>
-        <div className='border-b border-zinc-800 w-full flex justify-center pb-2'>Staking History</div>
+        <button
+          className={`cursor-pointer border-b w-full flex justify-center pb-2 ${staking?'text-primaryColor border-primaryColor':'border-zinc-800'}`}
+          onClick={() => handleClick("staking")}
+        >Staking Packages</button>
+        <button
+          className={`cursor-pointer border-b w-full flex justify-center pb-2 ${staking?'border-zinc-800 ':'text-primaryColor'}`}
+          onClick={()=>handleClick("history")}
+        >Staking History</button>
       </div>
     </div>
 
--- app/(menu)/staking/page.js
+++ app/(menu)/staking/page.js
@@ -1,14 +1,19 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import StakingNavbar from './components/StakingNavbar'
 import StakingPackage from './components/StakingPackage'
 import StakingHistory from './components/StakingHistory'
 
 export default function page() {
+  const [stakingPackage, setStakingPackage] = useState(true);
+  const handleClick = (tab)=>{
+    if(tab=='staking')setStakingPackage(true);
+    else setStakingPackage(false);
+  }
   return (
     <div>
-        <StakingNavbar/>
-        {/* <StakingPackage/> */}
-        <StakingHistory/>
+        <StakingNavbar handleClick = {handleClick} staking={stakingPackage}/>
+        {stakingPackage ?<StakingPackage/>: <StakingHistory/>}
     </div>
   )
 }
