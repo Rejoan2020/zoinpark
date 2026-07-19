c27325e43752d36da691724c0a198834e9577076
Rejoan2020 <rej.ash11@gmail.com>
2026-07-19 14:24:47 +0600

    home page design is changed

--- app/(dashboard)/(menu)/dashboard/components/Card.jsx
+++ app/(dashboard)/(menu)/dashboard/components/Card.jsx
@@ -3,13 +3,19 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Card(card) {
-  return (
-    <Link href={card.link} className='p-16 pt-8 pb-8'>
-      <div className='border border-zinc-800 rounded-md'>
-        <Image height={800} width={800} alt={card.title} src={card.url}/>
-        <div className='text-[10px] md:text-[12px] lg:text-[14px] xl:text-[16px] font-bold gradient p-2'>{card.title}</div>
-        <div className='text-secondaryText text-[10px] md:text-[12px] lg:text-[14px] xl:text-[16px] p-2'>{card.description}</div>
+  return ( 
+    <div className='bg-secondaryBackground m-8 border border-zinc-800 rounded-lg overflow-hidden'>
+      <Link href={card.link}>
+        <Image height={376} width={464} alt={card.title} src={card.url} className='w-full h-44 object-cover'/>
+      </Link>
+      <div className='p-4'>
+        <Link href={card.link} className='gradient font-bold'>
+          {card.title}
+        </Link>
+        <div className='text-secondaryText'>
+          {card.description}
+        </div>
       </div>
-    </Link>
+    </div>
   )
 }
--- app/(dashboard)/(menu)/dashboard/components/DashboardHome.jsx
+++ app/(dashboard)/(menu)/dashboard/components/DashboardHome.jsx
@@ -63,7 +63,7 @@ export default function DashboardHome({wallet}) {
 
         </div>
       </div>
-      <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2'>
         {cards.map((num) => <Card
           key={num.id}
           title={num.title}
