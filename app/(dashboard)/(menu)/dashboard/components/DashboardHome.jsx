e6548b4fb3d396b560200493eef1ab6d3d97277d
Rejoan2020 <rej.ash11@gmail.com>
2026-07-26 21:56:38 +0600

    fixes previously designed bar, replace the prev one with weekly challenges completion bar

--- app/(dashboard)/(menu)/dashboard/components/DashboardHome.jsx
+++ app/(dashboard)/(menu)/dashboard/components/DashboardHome.jsx
@@ -1,30 +1,32 @@
 import React from 'react'
 import Card from './Card'
 
-export default function DashboardHome({wallet}) {
+export default function DashboardHome({ wallet }) {
   const cards = [
     {
       id: 1,
-      title: 'Invite and Earn',
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      title: "Invite and Earn",
+      description: "Share your referral link with friends and earn rewards.",
       image: "/icons/home/invite.png",
       link: "/dashboard/invite"
     },
     {
       id: 2,
-      title: 'Weekly challenge',
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      title: "Weekly Challenge",
+      description: "Complete weekly tasks and claim exclusive ZOIN rewards before the week ends.",
       image: "/icons/home/weekly.png",
       link: "/dashboard/weekly-challenge"
     },
     {
       id: 3,
-      title: 'Community Events',
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      title: "Community Events",
+      description: "Join community events, and campaigns to earn bonus rewards and connect with other members.",
       image: "/icons/home/community.png",
       link: "/dashboard/community-events"
     }
   ];
+  let completed = 3;
+  let progress = completed * 33.33;
   return (
     <div className='mb-32'>
       <div className='xl:h-[247px] lg:h-[180px] md:h-[130px] flex xl:text-xl lg:text-l md:text-md border border-zinc-800'>
@@ -45,22 +47,23 @@ export default function DashboardHome({wallet}) {
             <ul className="text-[#4AE3C9] list-disc pl-5">
               <li></li>
             </ul>
-            <div className='gradient'>Airdrop Event | 100000000000</div>
+            <div className='gradient'>{`Weekly Challenges | ${completed}/3 Completed`}</div>
           </div>
           <div className='text-[10px] md:text-[12px] lg:text-[14px] xl:text-[16px]'>
-            <div className='bg-[#032C2D80] flex relative overflow-hidden md:h-[24px] lg:h-[32px] xl:h-[48px] h-[24px]'>
-              <div className='bg-[#4AE3C9] rounded-md p-4 flex items-center' style={{ width: '13.50%' }}>
-                <div className='' style={{ marginLeft: 'px' }}>13.50%</div>
+            <div className='bg-[#032C2D80] flex relative h-6 lg:h-10'>
+              <div className='bg-[#4AE3C9] rounded-md p-1 lg:p-2 flex items-center' style={{ width: `${progress}%` }}>
               </div>
               <div className='absolute right-4 top-1/2 -translate-y-1/2'>
                 <img className='' src='/icons/home/trophy.svg' />
               </div>
-            </div>
-
-            <div style={{ marginLeft: '12%' }}><img src='/icons/home/slidingArrow.svg' /></div>
-            <div className='text-primaryText' style={{ marginLeft: '12%' }}>2342342</div>
+              <div className={`${completed>1?'':'text-[#D4AF37]'} absolute top-1/2 -translate-y-1/2 left-1/4`}>
+                {completed} challenge{completed===1?'':'s'} completed!
+              </div>
+              
+            </div> 
+            <div style={{ marginLeft: `${progress-3}%` }}><img src='/icons/home/slidingArrow.svg' /></div>
+            {/* <div className='text-primaryText' style={{ marginLeft: `${progress}%` }}>{completed}</div> */}
           </div>
-
         </div>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2'>
@@ -69,7 +72,7 @@ export default function DashboardHome({wallet}) {
           title={num.title}
           description={num.description}
           url={num.image}
-          link = {num.link}
+          link={num.link}
         />)
         }
       </div>
